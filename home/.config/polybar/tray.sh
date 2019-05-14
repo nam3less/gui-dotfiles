bbd1286fee41af1842f4f189107fefb89c13ce29
nam3less <robin.m.beckmann@gmail.com>
2019-05-14 10:57:43 +0200

    Changed appearance slightly and added dunst

--- home/.config/polybar/tray.sh
+++ home/.config/polybar/tray.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+sleep 0.3
 cmst &
--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,9 +1,12 @@
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Spacing
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
+import XMonad.Util.Scratchpad
 
 import qualified DBus as D
 import qualified DBus.Client as D
@@ -13,51 +16,66 @@ import Graphics.X11.ExtraTypes.XF86
 import qualified Codec.Binary.UTF8.String as UTF8
 
 -- Colors
-fg        = "#ebdbb2"
-bg        = "#282828"
-gray      = "#a89984"
-bg1       = "#3c3836"
-bg2       = "#505050"
-bg3       = "#665c54"
-bg4       = "#7c6f64"
+fg         = "#ebdbb2"
+bg         = "#282828"
+gray       = "#ced4da"
+darkgrey   = "#495057"
+bg1        = "#3c3836"
+bg2        = "#505050"
+bg3        = "#665c54"
+bg4        = "#7c6f64"
 
-green     = "#b8bb26"
-darkgreen = "#98971a"
-red       = "#fb4934"
-darkred   = "#cc241d"
-yellow    = "#fabd2f"
-blue      = "#83a598"
-purple    = "#d3869b"
-aqua      = "#8ec07c"
-white     = "#eeeeee"
+green      = "#51cf66"
+darkgreen  = "#40c057"
+red        = "#ff8787"
+darkred    = "#fa5353"
+yellow     = "#ffd43b"
+darkyellow = "#fab005"
+blue       = "#4dadf7"
+darkblue   = "#228ae6"
+purple     = "#d3869b"
+aqua       = "#8ec07c"
+white      = "#e9ecef"
 
-pur2      = "#5b51c9"
-blue2     = "#2266d0"
+pur2       = "#5b51c9"
+blue2      = "#2266d0"
 
-
+-- Main configuration
 main = do
   dbus <- D.connectSession
-  D.requestName dbus (D.busName_ "org.xmonad.Log") 
+  D.requestName dbus (D.busName_ "org.xmonad.Log")
     [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
 
-  xmonad $ desktopConfig
-    { terminal    = "urxvtc"
+  xmonad $ docks $ ewmh desktopConfig
+    { terminal    = myTerminal
     , modMask     = mod4Mask
     , layoutHook  = myLayout
+    , manageHook  = myManageHook
     , startupHook = myStartupHook
     , logHook     = dynamicLogWithPP (myLogHook dbus)
     } `additionalKeysP` specialKeys
 
 myLayout = avoidStruts $ spacingRaw False (Border 5 5 5 5) True (Border 10 10 10 10) True $ layoutHook def
+myTerminal = "urxvtc"
+myFilemanager = "ranger"
+myBrowser = "firefox"
+myEditor = "emacs"
 
 specialKeys = [ ("<XF86MonBrightnessUp>", spawn $ modifyBacklightCmd "-inc 20")
               , ("<XF86MonBrightnessDown>", spawn $ modifyBacklightCmd "-dec 20")
 
-              , ("<XF86AudioRaiseVolume>", spawn $ modifyVolumeCmd "+5")
+              , ("<XF86AudioRaiseVolume>", spawn $ modifyVolumeCmd "+5%")
               , ("<XF86AudioLowerVolume>", spawn $ modifyVolumeCmd "-5%")
               , ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-              ]
 
+              , ("<XF86AudioMicMute>", spawn "pactl set-source-mute 1 toggle")
+
+              , ("M-s", scratchpadSpawnActionTerminal myTerminal)
+              , ("M-p", spawn "rofi -show drun")
+              , ("M-a f", spawn $ myTerminal ++ " -e " ++  myFilemanager)
+              , ("M-a b", spawn myBrowser)
+              , ("M-a e", spawn myEditor)
+              ]
 
 -- LogHook to communicate with Polybar
 myLogHook :: D.Client -> PP
@@ -66,12 +84,13 @@ myLogHook dbus = def
     , ppCurrent = wrap ("%{B" ++ bg2 ++ "} ") " %{B-}"
     , ppVisible = wrap ("%{B" ++ bg1 ++ "} ") " %{B-}"
     , ppUrgent = wrap ("%{F" ++ red ++ "} ") " %{F-}"
-    , ppHidden = wrap " " " "
+    , ppHidden = wrap " " " " . noScratchpad
     , ppWsSep = ""
     , ppSep = "  "
     , ppTitle = shorten 40
     , ppOrder = (\(ws:_:rest) -> ws : rest)
     }
+  where noScratchpad ws = if ws == "NSP" then "" else ws
 
 -- Emit a DBus signal on log updates
 dbusOutput :: D.Client -> String -> IO ()
@@ -85,8 +104,15 @@ dbusOutput dbus str = do
     interfaceName = D.interfaceName_ "org.xmonad.Log"
     memberName = D.memberName_ "Update"
 
+-- Manage hook
+myManageHook = manageDocks <+> manageScratchpad <+> manageHook desktopConfig
+
+manageScratchpad :: ManageHook
+manageScratchpad = scratchpadManageHook (W.RationalRect 0.20 0.20 0.6 0.6)
+
 -- Spawn system daemons
 myStartupHook = do
+  spawn "light-locker"
   spawn "setxkbmap -layout de -option caps:escape"
   spawn "compton"
   spawn "~/.config/polybar/polybar.sh && ~/.config/polybar/tray.sh"
